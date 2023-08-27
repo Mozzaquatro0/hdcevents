afeb202897f48be7440a013ca18b62c3ea982a88
Mozzaquatro <welintonmozzaquatro06@gmail.com>
2023-08-27 16:41:51 -0300

    estilizado a página de criar evento e salvando o form no db

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -3,5 +3,32 @@
 @section('title','Criar Evento')
 
 @section('content')
-<h1>Crie um evento</h1>
+
+<div id="event-createe-container" class="col-md-6 offset-md-3">
+    <h1>Crie um evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento</label>
+            <input type="text" name="title" id="title" class="form-control" placeholder="Nome do Evento">
+        </div>
+        <div class="form-group">
+            <label for="text">Cidade</label>
+            <input type="text" name="city" id="city" class="form-control" placeholder="Cidade do Evento">
+        </div>
+        <div class="form-group">
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="private">
+                <option value="0">Não</option>       
+                <option value="1">Sim</option>
+            </select> 
+        </div>
+        <div class="form-group">
+            <label for="title">Descrição</label>
+            <textarea name="description" id="description" placeholder="O quê vai acontecer no evento?" class="form-control"></textarea>
+        </div>
+        <input type="submit" value="Criar evento" class="btn btn-primary">
+    </form>
+</div>
+
 @endsection
